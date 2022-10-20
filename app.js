6c8d03c68150a8ac3c7faf93c36452aace9c789e
DiazPatricio <patriciodiaznqn@gmail.com>
2022-10-19 21:24:12 -0300

    1ra subida

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require ('express')
+
+const app = express()
+
+const path = require ('path')
+
+const publicpath = path.resolve('public')
+
+app.use(express.static(publicpath))
+
+const puerto = 3003
+
+
+app.listen(puerto, () => {console.log ('servidor funcionando')})
+
+
+app.get('/', (req, res) => {
+
+    res.sendFile (path.resolve('views/home.html'))})
+
+    app.get('/login', (req, res) => {
+
+        res.sendFile (path.resolve('views/login.html'))})
+
+        app.get('/register', (req, res) => {
+
+            res.sendFile (path.resolve('views/register.html'))})
+
+
+            
+
+
